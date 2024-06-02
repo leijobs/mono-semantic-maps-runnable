649ae619a0d58b7483d80abc42c20af036ce154e
leijobs <69192260+leijobs@users.noreply.github.com>
2024-06-02 16:50:32 +0800

    add code for fusion

--- src/nn/topdown.py
+++ src/nn/topdown.py
@@ -27,3 +27,26 @@ class TopdownNetwork(nn.Sequential):
 
         
         super().__init__(*modules)
+
+
+
+class FusionTopdownNetwork(nn.Sequential):
+
+    def __init__(self, in_channels, channels, layers=[6, 1, 1],
+                 strides=[1, 2, 2], blocktype='basic'):
+        modules = list()
+        self.downsample = 1
+        for nblocks, stride in zip(layers, strides):
+            # Add a new residual layer
+            module = ResNetLayer(
+                in_channels, channels, nblocks, 1 / stride, blocktype=blocktype)
+            modules.append(module)
+
+            # Halve the number of channels at each layer
+            in_channels = module.out_channels
+            channels = channels // 2
+            self.downsample *= stride
+
+        self.out_channels = in_channels
+
+        super().__init__(*modules)
